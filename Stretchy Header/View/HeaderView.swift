0a8f2056eee1d4e75cca47935da2185a3a5e842b
Bruno Silva <bruno.silva@bestseller.com>
2019-08-20 12:19:44 +0100

    Gradient layer implemented

--- Stretchy Header/View/HeaderView.swift
+++ Stretchy Header/View/HeaderView.swift
@@ -25,6 +25,8 @@ class HeaderView: UICollectionReusableView {
         
         //blur
         setupVisualEffectBlur()
+        //gradient layer
+        setupGradientLayer()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -41,6 +43,20 @@ class HeaderView: UICollectionReusableView {
             self?.addSubview(visualEffectView)
             visualEffectView.fillSuperview()
         })
+        animator.fractionComplete = 0
+    }
+    
+    fileprivate func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1]
+        gradientLayer.frame = self.bounds
+        gradientLayer.frame.origin.y -= self.bounds.height
+        
+        let gradientContainerView = UIView()
+        addSubview(gradientContainerView)
+        gradientContainerView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        gradientContainerView.layer.addSublayer(gradientLayer)
     }
     
 }
